c2251607b9d5926e1783a3cb79b92216711b8a7c
Tarun Narayanashetti <narayanashettitarun01@gmail.com>
2026-06-03 20:55:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.java
+++ Difficulty: Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.java
@@ -5,18 +5,21 @@ class Solution {
     int search(String pat, String txt) {
         // code here
         int freq[] = new int[26];
-        int ans = 0,unq = 0;
+        int unq = 0,ans = 0;
         for(char ch : pat.toCharArray()){
             if(freq[ch - 'a'] == 0)unq++;
-            freq[ch-'a']--;
+            freq[ch - 'a']--;
         }
-        int i = 0, j = 0;
-        while(j < txt.length()){
+        int i = 0,j = 0;
+        int n = txt.length(), k = pat.length();
+        while(j < n){
             int curr = txt.charAt(j) - 'a';
             if(freq[curr] == 0)unq++;
             freq[curr]++;
             if(freq[curr] == 0)unq--;
-            if(j - i + 1 < pat.length())j++;
+            if(j - i + 1 < k){
+                j++;
+            }
             else{
                 if(unq == 0)ans++;
                 int cur = txt.charAt(i) - 'a';
