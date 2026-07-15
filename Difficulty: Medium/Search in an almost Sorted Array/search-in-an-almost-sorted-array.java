5f41f930e175061b8f5b29cef9566036cb4c6592
Tarun Narayanashetti <narayanashettitarun01@gmail.com>
2026-07-15 07:40:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in an almost Sorted Array/search-in-an-almost-sorted-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findTarget(int arr[], int target) {
+        // code here
+        int low = 0, high = arr.length - 1;
+        while(low <= high){
+            int mid = low + (high -low)/2;
+            if(arr[mid] == target)return mid;
+            else if(mid - 1 >= low && arr[mid - 1] == target)return mid - 1;
+            else if(mid + 1 <= high && arr[mid + 1] == target)return mid + 1;
+            else if(arr[mid] < target) low = mid + 2;
+            else high = mid - 2;
+        }
+        return -1;
+    }
+}
