45a4a6bd7dda59774793af3327eb7e6720883090
Tarun Narayanashetti <narayanashettitarun01@gmail.com>
2026-08-11 19:09:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Set Bits/count-set-bits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int setBits(int n) {
+        // code here
+         int count = 0;
+        while (n > 0) {
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
+}
